daa981f72f3979d8f3a23e6c5531ef5e5e9cf8cc
Noureldein Hussein <56475893+Noureldein2000@users.noreply.github.com>
2025-03-19 00:27:56 +0200

    intro linq and sorting

---
+++ C# Advanced/LINQ/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Advanced.LINQ
+{
+    internal class Student<T>
+    {
+        public Student()
+        {
+            Ids = new List<T>();
+        }
+        public List<T> Ids { get; set; }
+        public void addId(T id) => Ids.Add(id);
+        public void removeId(T id) => Ids.Remove(id);
+
+        public IEnumerable<T> SortedIds(bool desc = false)
+        {
+            //Sorting
+            var ids = from id in Ids where desc == true orderby id descending select id;
+
+            var ids2 = desc ? Ids.OrderByDescending(s => s) : ids.OrderBy(s => s);
+            return ids2;
+        }
+
+        public IEnumerable<T> IdsReverse() => SortedIds(true).Reverse();
+
+
+    }
+}
--- C# Advanced/Program.cs
+++ C# Advanced/Program.cs
@@ -1,19 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using C__Advanced.Generics;
+using C__Advanced.LINQ;
 
 Console.WriteLine("Hello, World of Generics!\n");
 
 
-var genericList = new GenericList<string,double>();
+var genericList = new GenericList<string, double>();
 genericList.add("1");
 genericList.add("2");
 genericList.add("3");
 genericList.remove("1");
 
 Console.WriteLine($"count of items is {genericList.CountItem}\n");
-Console.WriteLine($"Sum of T2 {genericList.Sum(2,4)}");
-Console.WriteLine($"Sum of T2 {genericList.Sum(2.3,-4)}");
-Console.WriteLine($"Sum of T2 {genericList.Sum(2.3,39.2)}");
+Console.WriteLine($"Sum of T2 {genericList.Sum(2, 4)}");
+Console.WriteLine($"Sum of T2 {genericList.Sum(2.3, -4)}");
+Console.WriteLine($"Sum of T2 {genericList.Sum(2.3, 39.2)}");
 
 
 var genericList2 = new GenericList2<Person>();
+
+//------------------------------------- LINQ -------------------------------------
+var student = new Student<int>();
+student.addId(66);
+student.addId(24);
+
+
+Console.WriteLine($"print ids of student greater than 3 this is called (Deffered Execution)\n");
+foreach (var std in student.Ids.Where(x => x > 18).ToList())
+{
+    Console.Write(std + ",");
+}
+
+Console.WriteLine($"print ids  sorted of student (Deffered Execution)\n");
+foreach (var std in student.SortedIds(true))
+{
+    Console.Write(std + ",");
+}
+
+Console.WriteLine($"print ids  sorted Reversed of student (Deffered Execution)\n");
+foreach (var std in student.IdsReverse())
+{
+    Console.Write(std + ",");
+}
